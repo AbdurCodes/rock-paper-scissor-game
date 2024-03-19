9d86b938c203cbcaa7dc9ffd431e418d88fe94cc
AbdurCodes <124054847+AbdurCodes@users.noreply.github.com>
2024-03-20 03:39:26 +0500

    develop UI for rps game

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let roundResultHtml = document.querySelector(".roundResult");
+let scores = document.querySelector('.scores');
 
 function getComputerChoice (){
     let computerChoices = [ 'Rock', 'Paper', 'Scissor'];
@@ -9,65 +11,96 @@ let computerScore = 0;
 let userScore = 0;
 
 function playRound(computerSelection, playerSelection ){
-    
     if (playerSelection === 'paper'){
         if(computerSelection === 'Rock'){
             userScore += 1;
-            return "You Win! Paper beats Rock";
+            return "\nYour choice: Paper\nComputer's choice: Rock\nYou Win! Paper beats Rock";
         } else if (computerSelection === 'Scissor'){
             computerScore += 1;
-            return "You Lose! Scissor cuts Paper.";
+            return "\nYour choice: Paper\nComputer's choice: Scissor\nYou Lose! Scissor cuts Paper";
         } else {
-            return "It's a tie";
+            return "\nYour choice: Paper\nComputer's choice: Paper\nIt's a tie";
         }
     }
     else if (playerSelection === 'scissor'){
         if(computerSelection === 'Rock'){
             computerScore += 1;
-            return "You Lose! Rock beats Scissors";
+            return "\nYour choice: Scissor\nComputer's choice: Rock\nYou Lose! Rock beats Scissors";
         } else if (computerSelection === 'Paper'){
             userScore += 1;
-            return "You Win! Scissor cuts Paper.";
+            return "\nYour choice: Scissor\nComputer's choice: Paper\nYou Win! Scissor cuts Paper";
         } else {
-            return "It's a tie";
+            return "\nYour choice: Scissor\nComputer's choice: Scissor\nIt's a tie";
         }
     }
     else if (playerSelection === 'rock'){
         if(computerSelection === 'Scissor'){
             userScore += 1;
-            return "You Win! Rock beats Scissors";
+            return "\nYour choice: Rock\nComputer's choice: Scissor\nYou Win! Rock beats Scissors";
         } else if (computerSelection === 'Paper'){
             computerScore += 1;
-            return "You Lose! Paper beats Rock";
+            return "\nYour choice: Rock\nComputer's choice: Paper\nYou Lose! Paper beats Rock";
         } else {
-            return "It's a tie";
+            return "\nYour choice: Rock\nComputer's choice: Rock\nIt's a tie";
         }
     }
-    else{
-        userScore -= 1;
-        return "Allowed inputs: Rock, Paper, Scissor";
+}
+
+let rockBtn = document.querySelector('.rock');
+let paperBtn = document.querySelector('.paper');
+let scissorBtn = document.querySelector('.scissor');
+let chooseOptionText = document.querySelector('.rps-options-buttons');
+let replayBtn = document.querySelector('.replayBtn');
+let h2 = document.querySelector('h2');
+
+
+let counter = 1;
+
+let callback = (e)=>{
+    let computerChoice = getComputerChoice();
+    let target = e.target;
+    let userChoice = target.value;
+    let roundResult = playRound(computerChoice, userChoice);
+
+    let round = document.createElement('div');
+
+    round.innerText = (counter>1) ? `\nRound ${counter++}: ${roundResult}` : `Round ${counter++}: ${roundResult}`;
+    roundResultHtml.appendChild(round);
+
+    if (counter>5){
+        chooseOptionText.innerText = "Game over!";
+        removeEventListener('click', callback);
+        h2.style.display = 'none';
+        replayBtn.style.display = 'inline';
+        chooseOptionText.appendChild(replayBtn);
+        replayBtn.addEventListener('click', ()=>{
+            location.reload();
+        });
+        
+        let computerScoreHtml = document.createElement('div');
+        let userScoreHtml = document.createElement('div');
+        computerScoreHtml.textContent = `Computer score: ${computerScore}`;
+        userScoreHtml.textContent = `Your score: ${userScore}`;
+        scores.appendChild(computerScoreHtml);
+        scores.appendChild(userScoreHtml);
+
+        let finalResult = document.createElement('div');
+        finalResult.textContent = '';
+        if (userScore > computerScore) {
+            finalResult.textContent = "You Won!";
+        }else if (computerScore > userScore) {
+            finalResult.textContent = "You Lost!";
+        } else {
+            finalResult.textContent = "The match is a tie!";
+        }
+        scores.appendChild(finalResult);
     }
 }
 
+rockBtn.addEventListener('click', callback);
+paperBtn.addEventListener('click', callback);
+scissorBtn.addEventListener('click', callback);
 
-function playGame(){
-    for (let i=0; i<5; i++){
-        let computerChoice = getComputerChoice();
-        let userChoice = prompt("Plz choose one option: \n1. Rock\n2. Paper\n3. Scissor").toLowerCase();
-        let roundResult = playRound(computerChoice, userChoice);
-        console.log(`Round ${i+1}:`,roundResult);
-    }
-}
-
-playGame();
-
-console.log("Computer score: ", computerScore);
-console.log("User Score: ", userScore);
-
-if (userScore > computerScore) {
-    alert("You are the winner");
-}else if (computerScore > userScore) {
-    alert("You are the loser");
-} else {
-    alert("The match is a tie");
-}
+// rockBtn.removeEventListener('click', callback);
+// paperBtn.removeEventListener('click', callback);
+// scissorBtn.removeEventListener('click', callback);
